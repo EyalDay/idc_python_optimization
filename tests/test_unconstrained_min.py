5b2d0b1b8fecda7cc4c267c2fa26d217e62e6ba4
Eyal Dayan <eydayan@drivenets.com>
2021-06-24 20:19:32 +0300

    rename tests file

--- tests/test_unconstrained_min.py
+++ tests/test_unconstrained_min.py
@@ -16,28 +16,17 @@ logger = logging.getLogger(__name__)
 import numpy as np
 
 from tests.examples import f1, f2, f3, rosenbrock, my_linear_func
-from src.unconstrained_min import gradient_descent
+from src.unconstrained_min import line_search
 from src.utils import FUNC2STR
 
 
 class TestGradientDecsent(unittest.TestCase):
 
-    def _test_func_impl(self):
-        # f1 = x^2 + y^2
-        # f2 = 5x^2 + y^2
-
-        x = np.array([2, 2])
-        a, b = f1(x)
-        assert a == np.sum(np.power(x, 2))
-        assert np.allclose(2 * x, b)
-
-        a, b = f2(x)
-        y = np.power(x, 2)
-        assert a == 5 * y[0] + y[1]
-
     def _test_some_func(self, func):
         x_0 = np.array([1, 1])
         step_size = 0.1
+        step_size = 1
+
         max_iteration = 100
         step_tol = 1e-8
         obj_tolerance = 1e-12
@@ -49,8 +38,8 @@ class TestGradientDecsent(unittest.TestCase):
                     f'step size tolerance {step_size}\n'
                     f'objective function tolerance {obj_tolerance}')
 
-        is_sucess, last_point, X1, X2 = gradient_descent(f=func, x0=x_0, step_size=step_size, max_iter=max_iteration,
-                                                         obj_tol=obj_tolerance, param_tol=step_tol)
+        is_sucess, last_point, X1, X2 = line_search(f=func, x0=x_0, step_size=step_size, max_iter=max_iteration,
+                                                         obj_tol=obj_tolerance, param_tol=step_tol, dir_selection_method='nt')
         plot_path_contour(func, FUNC2STR[func], X1, X2)
         d = 'Success' if is_sucess else  'Failure'
         logger.info('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
@@ -59,7 +48,7 @@ class TestGradientDecsent(unittest.TestCase):
         assert is_sucess
 
     @unittest.expectedFailure
-    def test_lin_min(self):
+    def _test_lin_min(self):
         x_0 = np.array([1, 1])
         step_size = 0.01
         max_iteration = 100
@@ -77,9 +66,9 @@ class TestGradientDecsent(unittest.TestCase):
             'The only way this function can succeed, is if the tolerance parameters are higher than the step size\n'
             'This is misleading this the minimum point is not actually found')
 
-        is_sucess, last_point, X1, X2 = gradient_descent(f=my_linear_func, x0=x_0, step_size=step_size,
+        is_sucess, last_point, X1, X2 = line_search(f=my_linear_func, x0=x_0, step_size=step_size,
                                                          max_iter=max_iteration,
-                                                         obj_tol=obj_tolerance, param_tol=step_tol)
+                                                         obj_tol=obj_tolerance, param_tol=step_tol, dir_selection_method='nt')
         plot_path_contour(my_linear_func, FUNC2STR[my_linear_func], X1, X2)
         d = 'Success' if is_sucess else  'Failure'
         logger.info('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
@@ -96,6 +85,7 @@ class TestGradientDecsent(unittest.TestCase):
     def test_rosenbrock_min(self):
         x_0 = np.array([2, 2])
         step_size = 0.001
+        step_size = 1
         max_iteration = 10000
         step_tol = 1e-8
         obj_tolerance = 1e-7
@@ -106,9 +96,9 @@ class TestGradientDecsent(unittest.TestCase):
                     f'step size tolerance {step_size}\n'
                     f'objective function tolerance {obj_tolerance}')
 
-        is_sucess, last_point, X1, X2 = gradient_descent(f=rosenbrock, x0=x_0, step_size=step_size,
+        is_sucess, last_point, X1, X2 = line_search(f=rosenbrock, x0=x_0, step_size=step_size,
                                                          max_iter=max_iteration,
-                                                         obj_tol=obj_tolerance, param_tol=step_tol)
+                                                         obj_tol=obj_tolerance, param_tol=step_tol, dir_selection_method='nt')
 
         plot_path_contour(rosenbrock, FUNC2STR[rosenbrock], X1, X2)
 
